ef89318f02dd37976df81a53d0adc51522e152d2
João Mano <joaomanooo@gmail.com>
2016-06-02 15:07:36 +0100

    fix default route

--- config/routes.rb
+++ config/routes.rb
@@ -57,6 +57,8 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
    root 'static_pages#index'
 
+   match "*path" => "static_pages#index", via: [:get, :post]
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
